94edfa358c1f8af21d017b10dd67e4a8c5ab5cf1
Bartosz Krajka <bartosz.krajka@droidsonroids.pl>
2021-09-02 22:08:52 +0200

    Add New Day button - no logic yet

--- src/pages/MagicPage.tsx
+++ src/pages/MagicPage.tsx
@@ -1,12 +1,38 @@
 import MagicTable from "../components/MagicTable";
-import { Box } from "@material-ui/core";
+import { Box, Button, makeStyles } from "@material-ui/core";
+import AddBoxIcon from "@material-ui/icons/AddBox";
 
-const MagicPage = () => (
-  <Box m={5}>
-    <Box display="flex">
-      <MagicTable />
+const useStyles = makeStyles({
+  button: {
+    "&:focus": {
+      backgroundColor: "green",
+      transform: "scale(1.2)",
+      transition: "0.5s",
+    },
+  },
+});
+
+const MagicPage = () => {
+  const classes = useStyles();
+
+  return (
+    <Box m={5}>
+      <Box mb={3}>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<AddBoxIcon />}
+          className={classes.button}
+          autoFocus
+        >
+          New day
+        </Button>
+      </Box>
+      <Box display="flex">
+        <MagicTable />
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default MagicPage;
