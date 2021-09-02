142c8c7c636336321b7b9efcb16966448bbfeeba
Bartosz Krajka <bartosz.krajka@droidsonroids.pl>
2021-09-02 21:51:26 +0200

    Refactor - extract components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import MagicTable from "./components/MagicTable";
-import { Box, CssBaseline } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
+import MagicPage from "./pages/MagicPage";
+import Header from "./layout/Header";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  })
-);
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
 
 function App() {
   const classes = useStyles();
@@ -28,27 +15,8 @@ function App() {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Magic
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <Box m={5}>
-        <Box display="flex">
-          <MagicTable />
-        </Box>
-      </Box>
+      <Header />
+      <MagicPage />
     </div>
   );
 }
---
+++ src/pages/MagicPage.tsx
@@ -0,0 +1,12 @@
+import MagicTable from "../components/MagicTable";
+import { Box } from "@material-ui/core";
+
+const MagicPage = () => (
+  <Box m={5}>
+    <Box display="flex">
+      <MagicTable />
+    </Box>
+  </Box>
+);
+
+export default MagicPage;
