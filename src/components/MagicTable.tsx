b03e5a0b37a3c318552633f67f8826cc66e48f6b
Bartosz Krajka <bartosz.krajka@droidsonroids.pl>
2021-09-21 14:26:50 +0200

    Change Table library - from data-grid to mui-datatables

--- src/components/MagicTable.tsx
+++ src/components/MagicTable.tsx
@@ -1,38 +1,27 @@
-import { DataGrid, GridColDef, GridRowData } from "@material-ui/data-grid";
+import { GridRowData } from "@material-ui/data-grid";
 import { DATE_FORMAT } from "../datetime/datetime";
+import MUIDataTable from "mui-datatables";
+import { Dayjs } from "dayjs";
 
-function getDate(params: any) {
-  const d = params.getValue(params.id, "date");
-  return d.format(DATE_FORMAT);
+function formatDate(day: Dayjs) {
+  return day.format(DATE_FORMAT);
 }
 
-const columns: GridColDef[] = [
-  {
-    field: "formattedDate",
-    headerName: "Date",
-    width: 200,
-    valueGetter: getDate,
-    sortComparator: (v1, v2) => (v1 as string).localeCompare(v2 as string),
-  },
-  {
-    field: "weight",
-    headerName: "Weight",
-    width: 150,
-    editable: true,
-  },
-];
+const columns = ["Day", "Weight"];
+
+const options = {};
+
+const rowToData = (row: GridRowData) => [formatDate(row.date), row.weight];
 
 export const MagicTable = ({ rows }: { rows: GridRowData[] }) => {
+  const data = rows.map(rowToData);
   return (
-    <div style={{ height: 400, width: "100%" }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={90}
-        disableSelectionOnClick
-        rowHeight={30}
-      />
-    </div>
+    <MUIDataTable
+      title={"Magic"}
+      data={data}
+      columns={columns}
+      options={options}
+    />
   );
 };
 
